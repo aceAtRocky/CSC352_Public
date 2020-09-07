493cf73e4fd1b841dc8eeeafc8a6706705998c2c
Ace Olszowka (Rocky) <70549305+aceAtRocky@users.noreply.github.com>
2020-09-07 16:31:09 -0600

    Added DNA Program

---
+++ DNA/DnaStrand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        var replacements =
+          new Dictionary<char, char>
+          {
+              {'T','A'},
+              {'A','T'},
+              {'C','G'},
+              {'G','C'},
+          };
+
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var currentChar in dna)
+        {
+            sb.Append(replacements[currentChar]);
+        }
+
+        return sb.ToString();
+    }
+}
