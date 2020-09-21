8e1c8807747d54459a1fc3807b4a87e0258d35fe
Ace Olszowka (Rocky) <70549305+aceAtRocky@users.noreply.github.com>
2020-09-21 17:26:39 -0600

    Added NarcissisticNumber Kata

---
+++ NarcissisticNumber/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static bool Narcissistic(int value)
+    {
+        int numberOfDigits = value.ToString().Length;
+
+        double runningTotal = 0;
+
+        foreach (Char digit in value.ToString())
+        {
+            runningTotal += Math.Pow(int.Parse(digit.ToString()), numberOfDigits);
+        }
+
+        return runningTotal == value;
+    }
+}
