595e22d120ef8e27e2dea37d2918549aa9fdf1a2
Ace Olszowka (Rocky) <70549305+aceAtRocky@users.noreply.github.com>
2020-09-21 17:26:53 -0600

    Updated GCD with various implementations

--- GCD/Logic.cs
+++ GCD/Logic.cs
@@ -6,14 +6,40 @@ namespace GCD
     {
         public static long gcd(long x, long y)
         {
-            if (y == 0)
+            //if (y == 0)
+            //{
+            //    return x;
+            //}
+            //else
+            //{
+            //    long modXY = x % y;
+            //    return gcd(y, modXY);
+            //}
+
+            // Iterrative Brute Force
+            long result = 0;
+
+            for (long i = 1; i <= x; i++)
             {
-                return x;
+                if (x % i == 0 && y % i == 0)
+                {
+                    result = i;
+                }
             }
-            else
-            {
-                return gcd(y, x % y);
-            }
+
+            return result;
+
+            // Iterative Same Algorithm
+            //long gcd = 0;
+            //while (y != 0)
+            //{
+            //    long modXY = x % y;
+            //    gcd = y;
+            //    y = modXY;
+            //    x = gcd;
+            //}
+
+            //return gcd;
         }
     }
 }
--- GCD/UnitTests.cs
+++ GCD/UnitTests.cs
@@ -9,6 +9,7 @@
         [TestCase(1, 2, 1)]
         [TestCase(2, 4, 2)]
         [TestCase(30, 45, 15)]
+        [TestCase(45, 30, 15)]
         [TestCase(12962250, 12345, 12345)]
         [TestCase(1,long.MaxValue, 1)]
         public void gcd_test(long x, long y, long expected)
