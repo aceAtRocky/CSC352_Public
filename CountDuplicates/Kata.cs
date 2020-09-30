9d69b07d3e602eb8d99d878e68f0ba11310a84a4
Ace Olszowka (Rocky) <70549305+aceAtRocky@users.noreply.github.com>
2020-09-30 17:34:59 -0600

    More Changes

---
+++ CountDuplicates/Kata.cs
@@ -0,0 +1,28 @@
+﻿namespace CountDuplicates
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int DuplicateCount(string str)
+        {
+            var charsInString = str.AsEnumerable().Select(currentChar => char.ToLower(currentChar));
+
+            var result = new Dictionary<char, int>();
+
+            foreach (var charInString in charsInString)
+            {
+                if (!result.ContainsKey(charInString))
+                {
+                    result.Add(charInString, 0);
+                }
+
+                result[charInString]++;
+            }
+
+            return result.Where(kvp => kvp.Value > 1).Count();
+        }
+    }
+}
---
+++ CountDuplicates/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿namespace CountDuplicates
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class KataTest
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual(0, Kata.DuplicateCount(""));
+            Assert.AreEqual(0, Kata.DuplicateCount("abcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, Kata.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, Kata.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
