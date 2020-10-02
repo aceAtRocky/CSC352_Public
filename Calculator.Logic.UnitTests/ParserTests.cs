bf7f739afc31fd8a9c410aa739d21fe77e90e6a0
Ace Olszowka (Rocky) <70549305+aceAtRocky@users.noreply.github.com>
2020-10-02 17:28:47 -0600

    calculator project

---
+++ Calculator.Logic.UnitTests/ParserTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Calculator.Logic.UnitTests
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        [TestCase("1+1", 2)]
+        public void Parse_ValidInput(string input, int expected)
+        {
+            // Arrange
+            // Taken care of in the arguments
+
+            // Act
+            int actual = Parser.Parse(input);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
--- Calculator.Windows/Form1.cs
+++ Calculator.Windows/Form1.cs
@@ -21,5 +21,153 @@ namespace Calculator.Windows
         {
             this.textBox1.Text += "1";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "0";
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine($"Receieved {e.KeyCode}");
+
+            switch (e.KeyCode)
+            {
+                case Keys.D0:
+                case Keys.NumPad0:
+                    {
+                        button0_Click(sender, e);
+                        break;
+                    }
+                case Keys.D1:
+                case Keys.NumPad1:
+                    {
+                        button1_Click(sender, e);
+                        break;
+                    }
+                case Keys.D2:
+                case Keys.NumPad2:
+                    {
+                        button2_Click(sender, e);
+                        break;
+                    }
+                case Keys.D3:
+                case Keys.NumPad3:
+                    {
+                        button3_Click(sender, e);
+                        break;
+                    }
+                case Keys.D4:
+                case Keys.NumPad4:
+                    {
+                        button4_Click(sender, e);
+                        break;
+                    }
+                case Keys.D5:
+                case Keys.NumPad5:
+                    {
+                        button5_Click(sender, e);
+                        break;
+                    }
+                case Keys.D6:
+                case Keys.NumPad6:
+                    {
+                        button6_Click(sender, e);
+                        break;
+                    }
+                case Keys.D7:
+                case Keys.NumPad7:
+                    {
+                        button7_Click(sender, e);
+                        break;
+                    }
+                case Keys.D8:
+                    {
+                        if (e.Shift)
+                        {
+                            // Multiply
+                            ;
+                        }
+                        else
+                        {
+                            button8_Click(sender, e);
+                        }
+
+                        break;
+                    }
+                case Keys.NumPad8:
+                    {
+                        button8_Click(sender, e);
+                        break;
+                    }
+                case Keys.D9:
+                case Keys.NumPad9:
+                    {
+                        button9_Click(sender, e);
+                        break;
+                    }
+                case Keys.Oemplus:
+                    {
+                        if (e.Shift)
+                        {
+                            // Addition
+                            btnAdd_Click(sender, e);
+                        }
+
+                        break;
+                    }
+                default:
+                    {
+                        // Do Nothing
+                        break;
+                    }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "+";
+        }
     }
+
 }
