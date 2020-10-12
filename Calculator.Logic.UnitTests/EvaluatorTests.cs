a3ab78e6a39e72b72cb0afa8aa47b7881155e49f
Ace Olszowka (Rocky) <70549305+aceAtRocky@users.noreply.github.com>
2020-10-12 17:38:07 -0600

    Implement Evaluate

---
+++ Calculator.Logic.UnitTests/EvaluatorTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Calculator.Logic.UnitTests
+{
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EvaluatorTests
+    {
+        [TestCase("1 + 1", 2.0)]
+        [TestCase("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3", 3.0001220703125)]
+        public void EvaluateInfix_ValidInput(string input, double expected)
+        {
+            double actual = Evaluate.EvaluateInfix(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("1 1 +", 2.0)]
+        [TestCase("3 4 2 * 1 5 - 2 3 ^ ^ / +", 3.0001220703125)]
+        public void EvaluateRPN_ValidInput(string input, double expected)
+        {
+            double actual = Evaluate.EvaluateRPN(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
--- Calculator.Logic/Evaluate.cs
+++ Calculator.Logic/Evaluate.cs
@@ -1,21 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculator.Logic
+﻿namespace Calculator.Logic
 {
+    using System;
+    using System.Collections.Generic;
+
     public class Evaluate
     {
-        public double EvaluateRPN(string rpn)
+        public static double EvaluateRPN(string rpn)
         {
-            throw new NotSupportedException();
+            Stack<double> evaluatorStack = new Stack<double>();
+            string[] tokens = rpn.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var token in tokens)
+            {
+                if (token.Equals("+"))
+                {
+                    double b = evaluatorStack.Pop();
+                    double a = evaluatorStack.Pop();
+                    double result = a + b;
+                    evaluatorStack.Push(result);
+                }
+                else if (token.Equals("-"))
+                {
+                    double b = evaluatorStack.Pop();
+                    double a = evaluatorStack.Pop();
+                    double result = a - b;
+                    evaluatorStack.Push(result);
+                }
+                else if (token.Equals("/"))
+                {
+                    double b = evaluatorStack.Pop();
+                    double a = evaluatorStack.Pop();
+                    double result = a / b;
+                    evaluatorStack.Push(result);
+                }
+                else if (token.Equals("*"))
+                {
+                    double b = evaluatorStack.Pop();
+                    double a = evaluatorStack.Pop();
+                    double result = a * b;
+                    evaluatorStack.Push(result);
+                }
+                else if (token.Equals("^"))
+                {
+                    double b = evaluatorStack.Pop();
+                    double a = evaluatorStack.Pop();
+                    double result = Math.Pow(a, b);
+                    evaluatorStack.Push(result);
+                }
+                else
+                {
+                    double parsedDouble = double.Parse(token);
+                    evaluatorStack.Push(parsedDouble);
+                }
+            }
+
+            return evaluatorStack.Pop();
         }
 
-        public double EvaluateInfix(string infix)
+        public static double EvaluateInfix(string infix)
         {
-            throw new NotSupportedException();
+            string rpn = Parser.ConvertToRPN(infix);
+            return EvaluateRPN(rpn);
         }
     }
 }
