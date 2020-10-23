122e7c7a4698ec5839c933936e4a34d513fc9ff8
Ace Olszowka (Rocky) <70549305+aceAtRocky@users.noreply.github.com>
2020-10-23 17:29:21 -0600

    Initial stab at the map progam

---
+++ MapManager/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MapManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mapPictureBox = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.assetPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mapPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapPictureBox
+            // 
+            this.mapPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("mapPictureBox.Image")));
+            this.mapPictureBox.Location = new System.Drawing.Point(231, 12);
+            this.mapPictureBox.Name = "mapPictureBox";
+            this.mapPictureBox.Size = new System.Drawing.Size(785, 663);
+            this.mapPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mapPictureBox.TabIndex = 0;
+            this.mapPictureBox.TabStop = false;
+            this.mapPictureBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.mapPictureBox_DragDrop);
+            this.mapPictureBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.mapPictureBox_DragEnter);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(39, 224);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // assetPictureBox
+            // 
+            this.assetPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("assetPictureBox.Image")));
+            this.assetPictureBox.Location = new System.Drawing.Point(39, 82);
+            this.assetPictureBox.Name = "assetPictureBox";
+            this.assetPictureBox.Size = new System.Drawing.Size(121, 136);
+            this.assetPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.assetPictureBox.TabIndex = 2;
+            this.assetPictureBox.TabStop = false;
+            this.assetPictureBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.assetPictureBox_DragDrop);
+            this.assetPictureBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.assetPictureBox_DragEnter);
+            this.assetPictureBox.DragOver += new System.Windows.Forms.DragEventHandler(this.assetPictureBox_DragOver);
+            this.assetPictureBox.DragLeave += new System.EventHandler(this.assetPictureBox_DragLeave);
+            this.assetPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.assetPictureBox_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 687);
+            this.Controls.Add(this.assetPictureBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.mapPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mapPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mapPictureBox;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox assetPictureBox;
+    }
+}
+
---
+++ MapManager/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            assetPictureBox.AllowDrop = true;
+            mapPictureBox.AllowDrop = true;
+        }
+
+        private void assetPictureBox_DragLeave(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Drag Leave Encountered");
+        }
+
+        private void assetPictureBox_DragDrop(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("Drag Drop Encountered");
+
+        }
+
+        private void assetPictureBox_DragEnter(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("Drag Enter Encountered");
+
+        }
+
+        private void assetPictureBox_DragOver(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("Drag Over Encountered");
+
+        }
+
+        private void assetPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            assetPictureBox.DoDragDrop(assetPictureBox.Image, DragDropEffects.Copy);
+        }
+
+        private void mapPictureBox_DragEnter(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("Map: Drag Enter Event Encountered");
+        }
+
+        private void mapPictureBox_DragDrop(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("Map: Drag Drop Operation Completed");
+        }
+    }
+}
